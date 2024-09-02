76cda14cb9df3d1ae952370c1901024e186bffb8
jackson <jackson.wong617@gmail.com>
2024-09-02 16:21:11 +0800

    update-240902 - optimize pools

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -33,7 +33,7 @@
           <span class="footer-version">
             <span v-show="$route.fullPath.toLocaleLowerCase().includes('icdex')"
               >ICDexRouter v{{ version.router }}; ICDexPair v{{ version.pair }};
-              ICDexMaker v{{ version.maker }}; ICDexUI v2.1.9
+              ICDexMaker v{{ version.maker }}; ICDexUI v2.2.0
             </span>
           </span>
         </span>
